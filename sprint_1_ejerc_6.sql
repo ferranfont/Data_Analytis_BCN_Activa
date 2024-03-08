10fafe826b69da86b459fe37c33c77dc5f00c311
ferranfont <74493521+ferranfont@users.noreply.github.com>
2024-03-08 22:19:28 +0100

    Delete sprint_1_ejerc_6.sql

--- sprint_1_ejerc_6.sql
+++
@@ -1,7 +0,0 @@
-SELECT company.id,company_name, ROUND(avg(amount),2)
-FROM company
-INNER JOIN transaction
-ON transaction.company_id =company.id
-group by company_id
-order by avg(amount) desc
-limit 1
