22047942f179b9b70cb179e1160929acf90ebc93
ferranfont <74493521+ferranfont@users.noreply.github.com>
2024-03-08 22:18:42 +0100

    Delete sprint_1_ejerc_2.sql

--- sprint_1_ejerc_2.sql
+++
@@ -1,4 +0,0 @@
-# Sprint_1_ejerc_2
-# En la tabla company listar el nombre de la compania, el mail y el pais
-# Ordenado Ascendente por el nombre de la empresa
-SELECT company_name, email, country FROM company ORDER BY company_name ASC;
