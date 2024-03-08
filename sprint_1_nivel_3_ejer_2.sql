49f5bda4224682af5094daa3c260bba9386e3a92
ferranfont <74493521+ferranfont@users.noreply.github.com>
2024-03-08 22:20:28 +0100

    Delete sprint_1_nivel_3_ejer_2.sql

--- sprint_1_nivel_3_ejer_2.sql
+++
@@ -1,8 +0,0 @@
-# Ver que companias hicieron compras que llegaron a buen fin en tres fechas determinadas
-
-SELECT company_name, DATE(timestamp) as fecha 
-FROM transaction
-INNER JOIN company
-ON transaction.company_id = company.id
-WHERE declined = 0
-HAVING  fecha ='2022-02-13' OR fecha ='2022-02-28' OR fecha ='2022-03-16' 
