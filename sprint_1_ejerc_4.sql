57840129465facda9735c7567b0082a92c185097
ferranfont <74493521+ferranfont@users.noreply.github.com>
2024-03-08 22:19:07 +0100

    Delete sprint_1_ejerc_4.sql

--- sprint_1_ejerc_4.sql
+++
@@ -1,7 +0,0 @@
-# Queremos saber CUANTOS países han comprado, no cuales, ni tampoco cuantas transacciones,
-# sino contar los países que han comprado.
-SELECT count(DISTINCT country)
-FROM transaction
-INNER JOIN company ON company.id = transaction.company_id
-WHERE transaction.declined =0
-ORDER BY country ASC
