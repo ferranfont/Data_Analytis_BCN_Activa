7d2237aba5f4f143b3e00c56f2a3426ac8a44c0d
ferranfont <74493521+ferranfont@users.noreply.github.com>
2024-03-08 22:19:40 +0100

    Delete sprint_1_nivel_2_ejer_1.sql

--- sprint_1_nivel_2_ejer_1.sql
+++
@@ -1,7 +0,0 @@
-# Método para buscar si hay empresas (id) repetidas
-# Contamos el número de id que el codigo devuelve tras agrupalas
-# Exigimos que ese número tenga un valor mayor que 1
-
-SELECT id, company_name, count(id) FROM company 
-GROUP BY id 
-HAVING count(id)>1
