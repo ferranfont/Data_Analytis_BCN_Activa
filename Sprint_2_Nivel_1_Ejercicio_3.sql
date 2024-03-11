317eec58b76752fda03152ff2dd8af6ba0c01f8b
ferranfont <74493521+ferranfont@users.noreply.github.com>
2024-03-11 16:06:32 +0100

    Delete Sprint_2_Nivel_1_Ejercicio_3.sql

--- Sprint_2_Nivel_1_Ejercicio_3.sql
+++
@@ -1,8 +0,0 @@
-# Recuperación de Transacciones de una empresa cuyo nombre empezaba por C
-
-select * from transaction as t
-where exists(
-	select * from company as c
-	where c.id = t.company_id
-    and company_name like 'c%'
-);
