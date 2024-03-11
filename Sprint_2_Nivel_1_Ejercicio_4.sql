b59d00ced70332cb7fa9dee7818cc7ad7a8df446
ferranfont <74493521+ferranfont@users.noreply.github.com>
2024-03-11 16:06:42 +0100

    Delete Sprint_2_Nivel_1_Ejercicio_4.sql

--- Sprint_2_Nivel_1_Ejercicio_4.sql
+++
@@ -1,9 +0,0 @@
-# Empresas que están registradas pero que no tienen transaciones
-# Es decir, emmpresas que están en el listado de companias pero que no están en el listado de Transacciones.
-
-select * from company as c
-where not exists (
-	select * from transaction as t
-    where t.company_id = c.id
-);
-
